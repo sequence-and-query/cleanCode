13912ba9f811ab00e5effbfa7f08b72f6c0f8f80
sequence-and-query <lifeisvector@gmail.com>
2025-07-24 09:56:43 +0900

    [REFACTORING] Change some variables

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,8 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.mockito:mockito-core:5.18.0")
+    implementation("org.projectlombok:lombok:1.18.32")
 }
 
 tasks.test {
